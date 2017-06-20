9bfa122040c787e41da24f7b4c042c0f581953e4
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-06-20 14:07:54 -0400

    I4: color extension done

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,27 +1,35 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 SECRET_NUMBER = rand(100)
-number = SECRET_NUMBER
 
+@correct = false
+@wrong = false
+@way_too_wrong = false
 
 get '/' do
-  guess   = params["guess"].to_i
-  message = check_guess(guess, number)
-  erb :index, :locals => {:number => number, :message => message}
+  guess = params["guess"].to_i
+  message = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
 
-  def check_guess(guess, number)
-    if (guess > number) && (guess < (number + 5))
-      "Too high!"
-    elsif (guess > number) && (guess > (number + 5))
-      "Way too high!"
-    elsif (guess < number) && (guess > (number - 5))
-      "Too low!"
-    elsif (guess < number) && (guess < (number -5))
-      "Way too low!"
-    else
-      "You got it right!\n The SECRET NUMBER is #{number}"
+  def check_guess(guess)
+    if guess == SECRET_NUMBER
+      @correct = true
+      message = "You got it right\n the SECRET NUMBER is #{SECRET_NUMBER}"
+    elsif guess > SECRET_NUMBER + 5
+      @way_too_wrong = true
+      message = "Way to high!"
+    elsif guess < SECRET_NUMBER - 5
+      @way_too_wrong = true
+      message = "Way too low!"
+    elsif guess > SECRET_NUMBER
+      @wrong = true
+      message = "Too high!"
+    elsif guess < SECRET_NUMBER
+      @wrong = true
+      message =  "Too low!"
     end
   end
